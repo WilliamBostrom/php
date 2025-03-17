6780b118ad4130d8bedd9ccf1318be66869f0139
William Boström <133894488+WilliamBostrom@users.noreply.github.com>
2025-03-17 13:26:23 +0100

    Refactor PHP files to include header and footer includes for improved structure and maintainability

--- modern-php/restaurant/our-mission.php
+++ modern-php/restaurant/our-mission.php
@@ -1,23 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles/simple.css" />
-    <link rel="stylesheet" href="./styles/custom.css" />
-    <title>Culinary Cove &bull; Our mission</title>
-</head>
-<body>
-  <header class="header-with-background" style="background-image: url('images/pexels-julia-volk-5273044.jpg'); ">
-    <h1>Culinary Cove</h1>
-    <p>Your sanctuary for exceptional flavors</p>
-    <nav>
-      <a href="our-mission.php">Our mission</a>
-      <a href="ingredients.php">Ingredients</a>
-    </nav>
-  </header>
-
-  <main>
+<?php
+$pageTitle = "Our mission";
+include 'inc/header.inc.php'; ?>
     <h1>Our mission</h1>
     <p>
       At Culinary Cove, our core mission is to offer a sanctuary for 
@@ -45,10 +28,4 @@
       just a dish; it's a journey into a world of unique and exciting
       flavors.
     </p>
-  </main>
-
-  <footer>
-    <p>Culinary Cove's website.</p>
-  </footer>
-</body>
-</html>
+    <?php include 'inc/footer.inc.php'; ?>
